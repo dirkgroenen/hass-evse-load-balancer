8299df086a4d019d4ae946ff5efe590cfe72b8cb
andersop91 <andersop91@gmail.com>
2025-08-18 20:08:29 +0200

    Update zaptec_charger.py
    set_current_limit method adjusted to pass installation_id instead of device_id for zaptec charger, as current limit is restricted at zaptec installation entity. Requires change in zaptec integration (missing translation_key for charger entity)

--- custom_components/evse_load_balancer/chargers/zaptec_charger.py
+++ custom_components/evse_load_balancer/chargers/zaptec_charger.py
@@ -71,15 +71,24 @@ class ZaptecCharger(HaDevice, Charger):
         # https://github.com/dirkgroenen/hass-evse-load-balancer/issues/9
 
     async def set_current_limit(self, limit: dict[Phase, int]) -> None:
-        """Set the current limit for the Zaptec charger."""
-        entity_id = self._get_entity_id_by_translation_key(
-            ZaptecEntityMap.ChargingCurrent
-        )
+        """Set the current limit for the Zaptec installation."""
+        entity_id = self._get_entity_id_by_translation_key(ZaptecEntityMap.Charger)
+        state = self.hass.states.get(entity_id)
+        if not state or "installation_id" not in state.attributes:
+            _LOGGER.exception(
+            "installation_id not found in attributes for entity %s", entity_id
+            )
+        installation_id = state.attributes["installation_id"]
+
+        _LOGGER.debug("Setting current limit to %s A", min(limit.values()))
+        _LOGGER.debug("Domain: %s, Service: %s, Installation Entity: %s, Value: %s",
+                      CHARGER_DOMAIN_ZAPTEC, ZAPTEC_SERVICE_LIMIT_CURRENT, installation_id, min(limit.values()))
+        
         await self.hass.services.async_call(
             domain=CHARGER_DOMAIN_ZAPTEC,
             service=ZAPTEC_SERVICE_LIMIT_CURRENT,
             service_data={
-                "device_id": entity_id,
+                "installation_id": installation_id,
                 "value": int(min(limit.values())),
             },
             blocking=True,
